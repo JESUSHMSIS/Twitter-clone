96cc1ba493d7d91b43bbc71d86d937405c1a220a
JESUSHMSIS <jeshuanaco7@gmail.com>
2025-10-16 14:30:09 -0400

    add new sidebar components

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,13 @@
 import { BsBellFill, BsHouseFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import { BiLogOut } from "react-icons/bi";
 
 const Sidebar = () => {
   const items = [
     {
-      label: "Home",
+      label: "Inicio",
       href: "/dashboard",
       icon: BsHouseFill,
     },
@@ -14,12 +17,33 @@ const Sidebar = () => {
       icon: BsBellFill,
     },
     {
-      label: "Profile",
+      label: "Perfil",
       href: "/user/123",
       icon: FaUser,
     },
   ];
-  return <div>sidebar</div>;
+  return (
+    <div className="col-sapn-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              label={item.label}
+              icon={item.icon}
+              href={item.href}
+            />
+          ))}
+          <SidebarItem
+            icon={BiLogOut}
+            label="Cerrar sesion"
+            onClick={() => {}}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
---
+++ src/components/SidebarItem.tsx
@@ -0,0 +1,29 @@
+import { IconType } from "react-icons";
+
+interface SidebarItemProps {
+  icon: IconType;
+  label: string;
+  href?: string;
+  onClick?: () => void;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  label,
+  href,
+  onClick,
+}) => {
+  return (
+    <div className="flex flex-row items-center">
+      <div className="relative rounded-full h-14 w-14 flex items-center justify-center p-4 hover:bg-slate-300 hover:bg-slate-300/20 cursor-pointer lg:hidden">
+        <Icon size={28} color="white" />
+      </div>
+      <div className="relative hidden lg:flex items-center p-4 gap-4 rounded-full hover:bg-slate-200 hover:bg-slate-200/20 cursor-pointer">
+        <Icon size={24} color="white" />
+        <p className="hidden lg:block text-white text-xl">{label}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarItem;
