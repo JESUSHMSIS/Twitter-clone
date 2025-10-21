de0f40da3d6aca744772ee20247e9c0a1a234fc0
JESUSHMSIS <jeshuanaco7@gmail.com>
2025-10-20 21:33:53 -0400

    fixed error types

--- src/components/FollowBar.tsx
+++ src/components/FollowBar.tsx
@@ -1,5 +1,9 @@
 "use client";
-
+interface ModelUser {
+  id: string;
+  name: string;
+  username: string;
+}
 import useUsers from "@/hooks/useUsers";
 import Avatar from "./ui/Avatar";
 const FollowBar = () => {
@@ -9,9 +13,9 @@ const FollowBar = () => {
   return (
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
-        <h2 className="text-white  text-xl font-semibold">A quien siguir</h2>
+        <h2 className="text-white  text-xl font-semibold">A quien seguir</h2>
         <div className="flex flex-col gap-6 mt-4">
-          {users.map((user: Record<string, any>) => (
+          {users.map((user: ModelUser) => (
             <div key={user?.id} className="flex flex-row gap-4">
               <Avatar userId={user.id} />
               <div className="flex flex-col">
